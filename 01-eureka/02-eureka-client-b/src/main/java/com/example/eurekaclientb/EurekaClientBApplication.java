120efc5e7c2b6c9c4c72763d61799ea5e28bec2d
Dinny浅风 <17501606708@qq.com>
2022-11-20 02:13:47 +0800

    first

---
+++ 01-eureka/02-eureka-client-b/src/main/java/com/example/eurekaclientb/EurekaClientBApplication.java
@@ -0,0 +1,16 @@
+package com.example.eurekaclientb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dinny-xu
+ */
+@SpringBootApplication
+public class EurekaClientBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientBApplication.class, args);
+    }
+
+}
