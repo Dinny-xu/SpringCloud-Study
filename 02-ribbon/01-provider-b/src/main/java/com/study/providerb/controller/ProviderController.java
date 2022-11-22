20e9b7178638ec8d5d445dc6fd792f86267debb4
Dinny浅风 <yanyan.xu@sipingsoft.com>
2022-11-22 21:23:53 +0800

    feat(ribbon): 负载均衡算法

---
+++ 02-ribbon/01-provider-b/src/main/java/com/study/providerb/controller/ProviderController.java
@@ -0,0 +1,17 @@
+package com.study.providerb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dinny-xu
+ */
+@RestController
+public class ProviderController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "我是提供者B的接口";
+    }
+
+}
