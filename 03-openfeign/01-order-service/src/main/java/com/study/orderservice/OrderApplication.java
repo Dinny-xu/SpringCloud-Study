d2eb1b30578788bd793c86a0f7b82dd234be35ea
Dinny浅风 <17501606708@qq.com>
2022-11-29 16:37:23 +0800

    feat(openfeign): 创建openfeign服务

---
+++ 03-openfeign/01-order-service/src/main/java/com/study/orderservice/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.study.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dinny-xu
+ */
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
